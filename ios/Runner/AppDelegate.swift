f6d50aa4751df647649448f37472458b1427df62
hieu tran <hieupro1106@gmail.com>
2021-12-15 23:51:11 +0700

    git commit -m "my changes"

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
+import google_mobile_ads
 import UIKit
 import Flutter
 
-import google_mobile_ads
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,35 +9,66 @@ import google_mobile_ads
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
     GeneratedPluginRegistrant.register(with: self)
 
-    let listTileFactory = ListTileNativeAdFactory()
-    FLTGoogleMobileAdsPlugin.registerNativeAdFactory(
-        self, factoryId: "listTile", nativeAdFactory: listTileFactory)
+    let nativeAdFactory:NativeAdFactoryExample! = NativeAdFactoryExample()
+
+    FLTGoogleMobileAdsPlugin.registerNativeAdFactory(self,
+                                                factoryId:"listTile",
+                                                nativeAdFactory:nativeAdFactory)
+
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
 
-class ListTileNativeAdFactory : FLTNativeAdFactory {
+// The UnifiedNativeAdView.xib and example GADUnifiedNativeAdView is provided and
+// explained by https://developers.google.com/admob/ios/native/advanced.
+class NativeAdFactoryExample : NSObject, FLTNativeAdFactory {
 
-    func createNativeAd(_ nativeAd: GADNativeAd,
-                        customOptions: [AnyHashable : Any]? = nil) -> GADNativeAdView? {
-        let nibView = Bundle.main.loadNibNamed("ListTileNativeAdView", owner: nil, options: nil)!.first
-        let nativeAdView = nibView as! GADNativeAdView
+    func createNativeAd(_ nativeAd:GADUnifiedNativeAd, customOptions:[AnyHashable : Any]? = nil) -> GADNativeAdView? {
+      // Create and place ad in view hierarchy.
+      let nibView = Bundle.main.loadNibNamed("NativeAdView", owner: nil, options: nil)?.first
+      guard let nativeAdView = nibView as? GADNativeAdView else {
+        return nil
+      }
 
-        (nativeAdView.headlineView as! UILabel).text = nativeAd.headline
+      // Associate the native ad view with the native ad object. This is
+      // required to make the ad clickable.
+      nativeAdView.nativeAd = nativeAd
 
-        (nativeAdView.bodyView as! UILabel).text = nativeAd.body
-        nativeAdView.bodyView!.isHidden = nativeAd.body == nil
+      // Populate the native ad view with the native ad assets.
+      // The headline is guaranteed to be present in every native ad.
+      (nativeAdView.headlineView as! UILabel).text = nativeAd.headline
 
-        (nativeAdView.iconView as! UIImageView).image = nativeAd.icon?.image
-        nativeAdView.iconView!.isHidden = nativeAd.icon == nil
+      // These assets are not guaranteed to be present. Check that they are before
+      // showing or hiding them.
+      (nativeAdView.bodyView as? UILabel)?.text = nativeAd.body
+        nativeAdView.bodyView?.isHidden = nativeAd.body == nil
 
+      (nativeAdView.callToActionView as? UIButton)?.setTitle(nativeAd.callToAction,
+                                                       for: .normal)
+        nativeAdView.callToActionView?.isHidden = nativeAd.callToAction == nil
+
+      (nativeAdView.iconView as? UIImageView)?.image = nativeAd.icon?.image
+        nativeAdView.iconView?.isHidden = nativeAd.icon == nil
+
+      (nativeAdView.storeView as? UILabel)?.text = nativeAd.store
+        nativeAdView.storeView?.isHidden = nativeAd.store == nil
+
+      (nativeAdView.priceView as? UILabel)?.text = nativeAd.price
+        nativeAdView.priceView?.isHidden = nativeAd.price == nil
+
+      (nativeAdView.advertiserView as? UILabel)?.text = nativeAd.advertiser
+        nativeAdView.advertiserView?.isHidden = nativeAd.advertiser == nil
+
+      // In order for the SDK to process touch events properly, user interaction
+      // should be disabled.
         nativeAdView.callToActionView?.isUserInteractionEnabled = false
 
-        nativeAdView.nativeAd = nativeAd
-
-        return nativeAdView
+      return nativeAdView
     }
 }
