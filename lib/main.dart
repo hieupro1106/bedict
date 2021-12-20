12558c1d4032a131d4457a5c6449c588361a5339
hieu tran <hieupro1106@gmail.com>
2021-12-20 21:12:23 +0700

    git commit -m "my changes"

--- lib/main.dart
+++ lib/main.dart
@@ -2670,7 +2670,7 @@ class WriteWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    margin: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(5),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: const BorderRadius.all(
@@ -2725,7 +2725,7 @@ class WriteWidget extends StatelessWidget {
                   c.isVip.value?
                     const SizedBox():
                     Container(
-                      margin: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: const BorderRadius.all(
@@ -2743,12 +2743,12 @@ class WriteWidget extends StatelessWidget {
                       // alignment: Alignment.center,
                       width: MediaQuery.of(context).size.width > 420? 180: (MediaQuery.of(context).size.width-60)/2,
                       height: MediaQuery.of(context).size.width > 420? 180*0.78125: (MediaQuery.of(context).size.width-60)*0.78125/2,
-                      child: const BannerAdWidget(adWidth: 180.0)
-                      // child: BannerAdWidget(adWidth: MediaQuery.of(context).size.width > 420? 180: (MediaQuery.of(context).size.width-60)/2)
+                      // child: const BannerAdWidget(adWidth: 180.0)
+                      child: BannerAdWidget(adWidth: MediaQuery.of(context).size.width > 420? 180: (MediaQuery.of(context).size.width-60)/2)
                     ),
                   for (int index=1; index<c.mean.length; index++)
                     Container(
-                      margin: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: const BorderRadius.all(
@@ -3089,7 +3089,7 @@ class PronunWidget extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                        margin: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.all(0),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: const BorderRadius.all(
@@ -3144,7 +3144,7 @@ class PronunWidget extends StatelessWidget {
                     c.isVip.value?
                       const SizedBox():
                       Container(
-                          margin: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.all(0),
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: const BorderRadius.all(
@@ -3166,7 +3166,7 @@ class PronunWidget extends StatelessWidget {
                       ),
                     for (int index=1; index<c.mean.length; index++)
                       Container(
-                          margin: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.all(0),
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: const BorderRadius.all(
