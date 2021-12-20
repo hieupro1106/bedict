55a08c551957933a25d8ef9f62a0956d05683d6f
hieu tran <hieupro1106@gmail.com>
2021-12-20 19:40:16 +0700

    git commit -m "my changes"

--- lib/main.dart
+++ lib/main.dart
@@ -1010,6 +1010,7 @@ class Home extends StatelessWidget {
                       ),
                   ),
                   const SizedBox(height:5),
+                  BannerAdWidget(adWidth: 150),
                   Container(
                     width: c.imageWidth.value - 10,
                     margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
@@ -2669,6 +2670,7 @@ class WriteWidget extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  BannerAdWidget(adWidth: 320),
                   Container(
                     margin: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
@@ -2743,7 +2745,7 @@ class WriteWidget extends StatelessWidget {
                       // alignment: Alignment.center,
                       width: MediaQuery.of(context).size.width > 420? 180: (MediaQuery.of(context).size.width-60)/2,
                       height: MediaQuery.of(context).size.width > 420? 180*0.78125: (MediaQuery.of(context).size.width-60)*0.78125/2,
-                      child: BannerAdWidget(adWidth: 320)
+                      child: BannerAdWidget(adWidth: 150)
                       // child: BannerAdWidget(adWidth: MediaQuery.of(context).size.width > 420? 180: (MediaQuery.of(context).size.width-60)/2)
                     ),
                   for (int index=1; index<c.mean.length; index++)
